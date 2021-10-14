29fb831a8167ca03e3ecba3d01a44f4a2ab083c6
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-10-14 08:41:03 +0530

    Update Badwordz.js

--- plugins/Badwordz.js
+++ plugins/Badwordz.js
@@ -1,8 +1,7 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 ws virus Fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
-Wa.me/+94766598862
+VIRUS - FUCKER
 */
 
 const Teenu = require('../events');
