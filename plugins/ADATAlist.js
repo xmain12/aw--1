a321e43d44b85aad69182708ed8396749b9d905e
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-10-14 08:33:37 +0530

    Update ADATAlist.js

--- plugins/ADATAlist.js
+++ plugins/ADATAlist.js
@@ -1,7 +1,7 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 ws virus Fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+VIRUS - FUCKER
 */
 
 const Alexa = require('../events');
