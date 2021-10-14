39a3ba0870df2672a1e24f4c25670c5afbabc530
FIRUS _FUCKER <88616416+Kaweeshachamodk@users.noreply.github.com>
2021-10-14 08:46:02 +0530

    Update Esthetic.js

--- plugins/Esthetic.js
+++ plugins/Esthetic.js
@@ -1,10 +1,10 @@
-/* Copyright (C) 2021 TENUX-Neotro.
+/* Copyright (C) 2021 ws virus Fucker.
 Licensed under the  GPL-3.0 License;
 you may not use this file except in compliance with the License.
-NEOTROX - TEENUHX
+VIRUS - FUCKER
 */
 
-const Asena = require('../events');
+const Stefanie = require('../events');
 const { MessageType, MessageOptions, Mimetype } = require('@adiwajshing/baileys');
 const fs = require('fs');
 const Ln = "Esthetic Photos"
@@ -16,7 +16,7 @@ let Lang = Language.getString('amazone');
 
 if (Config.WORKTYPE == 'public') {
 
-    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: false, desc: Lang.WALL }, (async (message, match) => {
+    Stefanie.addCommand({ pattern: 'esthetic ?(.*)', fromMe: false, desc: Lang.WALL }, (async (message, match) => {
       
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/estetikpic?apikey=THEVINDUapi`, { responseType: 'arraybuffer' })
 
@@ -28,7 +28,7 @@ if (Config.WORKTYPE == 'public') {
 
 else if (Config.WORKTYPE == 'private') {
 
-    Asena.addCommand({ pattern: 'esthetic ?(.*)', fromMe: true, desc: Lang.WALL }, (async (message, match) => {
+    Stefanie.addCommand({ pattern: 'esthetic ?(.*)', fromMe: true, desc: Lang.WALL }, (async (message, match) => {
       
         var ttinullimage = await axios.get(`https://api.zeks.xyz/api/estetikpic?apikey=THEVINDUapi`, { responseType: 'arraybuffer' })
 
